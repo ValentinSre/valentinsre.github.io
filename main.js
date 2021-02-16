71b67e0773c3c0223ab4b409501e6b5778505dc6
Valentin <73347253+ValentinSre@users.noreply.github.com>
2021-02-16 16:58:29 +0100

    Update main.js

--- main.js
+++ main.js
@@ -17,10 +17,8 @@ if (window.Worker) {
 	}
 
 	myWorker.onmessage = function(e) {
-		  console.log('Message reçu depuis le script principal.');
-		  var workerResult = 'Résultat : ' + (e.data[0] * e.data[1]);
-		  console.log('Envoi du message de retour au script principal');
-		  postMessage(workerResult);
+		result.textContent = e.data;
+		console.log('Message received from worker');
 }
 } else {
 	console.log('Your browser doesn\'t support web workers.')
