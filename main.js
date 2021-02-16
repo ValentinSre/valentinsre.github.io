d68966042c39cfc284f50afca094c81aa768a478
Valentin <73347253+ValentinSre@users.noreply.github.com>
2021-02-16 17:18:25 +0100

    Update main.js

--- main.js
+++ main.js
@@ -2,6 +2,10 @@ const first = document.querySelector('#x');
 const second = document.querySelector('#y');
 
 if (window.Worker) {
+		
+	const myWorker = new Worker("worker.js");
+
+	
 	first = getRndInteger(0,150);
 	console.log('x est changée')
 	second = getRndInteger(0,150);
@@ -10,9 +14,3 @@ if (window.Worker) {
 	console.log('Your browser doesn\'t support web workers.')
 }
 
-
-<script>
-  function getRndInteger(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-  }
-</script> 
