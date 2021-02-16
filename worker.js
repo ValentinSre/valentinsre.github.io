cca5aea58a165679e321d046538388ba5e0a22f6
Valentin <73347253+ValentinSre@users.noreply.github.com>
2021-02-16 17:17:53 +0100

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,11 +1,3 @@
-onmessage = function(e) {
-  console.log('Worker: Message received from main script');
-  const result = parseInt(e.data[0]) * parseInt(e.data[1]);
-  if (isNaN(result)) {
-    postMessage('Please write two numbers');
-  } else {
-    const workerResult = 'Result: ' + result;
-    console.log('Worker: Posting message back to main script');
-    postMessage(workerResult);
+onmessage = function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
   }
-}
